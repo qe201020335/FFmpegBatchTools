9a363ba9c75dbc3f5362d6a2c9aa2345cb722523
Sky Z <qe201020335@sina.com>
2023-01-01 17:16:03 -0500

    new: shared batch file processing

--- FFmpegBatchTools.Shared/ProgramBase.cs
+++ FFmpegBatchTools.Shared/ProgramBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
 using Newtonsoft.Json;
 
@@ -16,8 +17,9 @@ namespace FFmpegBatchTools.Shared
         
         private TConfig? _config = null;
 
-        protected bool _useSelectedDir = false;
-        private string? _selectedDir;
+        protected bool UseSelectedDir = false;
+        protected string? SelectedDir;
+        
         private FolderBrowserDialog _outputDirDialog = new FolderBrowserDialog();
 
         protected ProgramBase(string prgName, string confName, string confPath, string programDir, Func<TConfig>? newConfig = null)
@@ -29,7 +31,23 @@ namespace FFmpegBatchTools.Shared
             
             DealWithConfig(newConfig);
         }
-        
+
+        protected ProgramBase()
+        {
+            var ass = Assembly.GetCallingAssembly();
+            var prgName = ass.GetName().Name;
+            var confName = prgName + ".json";
+            var programDir = Path.GetDirectoryName(ass.Location) ?? "";
+            var confPath = Path.Combine(programDir, confName); 
+            
+            PrgName = prgName;
+            ConfName = confName;
+            ConfPath = confPath;
+            ProgramDir = programDir;
+            
+            DealWithConfig(null);
+        }
+
         private void DealWithConfig(Func<TConfig>? newConfig)
         {
             if (File.Exists(ConfPath))
@@ -48,8 +66,8 @@ namespace FFmpegBatchTools.Shared
                 if (_outputDirDialog.ShowDialog() == DialogResult.OK)
                 {
                     Console.WriteLine(_outputDirDialog.SelectedPath);
-                    _useSelectedDir = true;
-                    _selectedDir = _outputDirDialog.SelectedPath;
+                    UseSelectedDir = true;
+                    SelectedDir = _outputDirDialog.SelectedPath;
                     Configuration.LastOutputDir = _outputDirDialog.SelectedPath;
                 }
             }
@@ -61,5 +79,70 @@ namespace FFmpegBatchTools.Shared
                 serializer.Serialize(file, Configuration);
             }
         }
+
+        protected abstract bool Run(string inPath);
+
+        protected bool BatchRun(string[] args)
+        {
+            var len = args.Length;
+            if (len == 0)
+            {
+                MessageBox.Show("Drag video files on this tool.", PrgName);
+                return false;
+            }
+
+            foreach (var arg in args)
+            {
+                try
+                {
+                    if (!File.Exists(arg))
+                    {
+                        Console.WriteLine(arg + " DOES NOT EXIST");
+                        continue; // not exist
+                    }
+
+                    var fullpath = Path.GetFullPath(arg);
+                    if (!Run(fullpath))
+                    {
+                        return false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    return false;
+                }
+            }
+
+            Console.Write("\a");
+            MessageBox.Show("Done.", PrgName);
+            return true;
+        }
+
+        // protected void PostProcess(FileInfo inFile, FileInfo outFile)
+        // {
+        //     if (Configuration.CopyModifiedTime)
+        //     {
+        //         outFile.LastWriteTime = inFile.LastWriteTime;
+        //     }
+        //
+        //     var inPath = inFile.FullName;
+        //
+        //     if (Configuration.DeleteOriginal)
+        //     {
+        //         inFile.Delete();
+        //         Console.WriteLine($"old file deleted: {inPath}");
+        //     }
+        //
+        //     if (Configuration.CopyFileName)
+        //     {
+        //         var newName = Path.Combine(outParent, nameWithoutExtension + extension);
+        //         if (Configuration.DeleteOriginal || newName != inPath)
+        //         {
+        //             Console.WriteLine("Rename to old file name");
+        //             outFile.MoveTo(newName);
+        //         }
+        //     }
+        // }
     }
 }
---
+++ FFmpegBatchTools.Shared/Utils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace FFmpegBatchTools.Shared
+{
+    public class Utils
+    {
+        public static int StartProcess(string exe, string arguments)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = exe,
+                    Arguments = arguments,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardError = true,
+                }
+            };
+            process.Start();
+            process.OutputDataReceived += (sender, args) => Console.WriteLine(args.Data);
+            process.ErrorDataReceived += (sender, args) => Console.WriteLine(args.Data);
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit(-1);
+            
+            if (process.ExitCode != 0)
+            {
+                Console.WriteLine($"{process.StartInfo.FileName} Exited with error!");
+                switch (MessageBox.Show($"{process.StartInfo.FileName} Exited with error! Continue?", "Error!", MessageBoxButtons.YesNo))
+                {
+                    case DialogResult.Yes:
+                        Console.WriteLine("Continue.");
+                        return 1;
+                    case DialogResult.No:
+                        Console.WriteLine("Abort.");
+                        return -1;
+                    default:
+                        return 0;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
