9348225e7f46320af12b17ad6bd8efb873b6f808
Sky Z <qe201020335@sina.com>
2023-01-01 15:30:08 -0500

    new: Extract common shared

---
+++ FFmpegBatchTools.Shared/ConfigurationBase.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace FFmpegBatchTools.Shared
+{
+    public abstract class ConfigurationBase
+    {
+        [JsonProperty("DeleteOriginal")] public bool DeleteOriginal = false;
+
+        [JsonProperty("CopyModifiedTime")] public bool CopyModifiedTime = false;
+
+        [JsonProperty("CopyFileName")] public bool CopyFileName = true;
+
+        [JsonProperty("SelectOutputDir")]
+        public bool SelectOutputDir = false;
+
+        [JsonProperty("UseCustomFileExtension")]
+        public bool UseCustomFileExtension = true;
+
+        [JsonProperty("CustomFileExtension")]
+        public string CustomFileExtension = ".mp4";
+
+        [JsonProperty("LastOutputDir")]
+        public string LastOutputDir = "";
+
+        [JsonProperty("ExtraArguments")]
+        public string ExtraArguments = "";
+    }
+}
---
+++ FFmpegBatchTools.Shared/ProgramBase.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace FFmpegBatchTools.Shared
+{
+    public abstract class ProgramBase<TConfig> where TConfig : ConfigurationBase, new()
+    {
+        protected readonly string PrgName;
+        protected readonly string ConfName;
+        protected readonly string ConfPath;
+        protected readonly string ProgramDir;
+
+        protected TConfig Configuration => _config!;
+        
+        private TConfig? _config = null;
+
+        protected bool _useSelectedDir = false;
+        private string? _selectedDir;
+        private FolderBrowserDialog _outputDirDialog = new FolderBrowserDialog();
+
+        protected ProgramBase(string prgName, string confName, string confPath, string programDir, Func<TConfig>? newConfig = null)
+        {
+            PrgName = prgName;
+            ConfName = confName;
+            ConfPath = confPath;
+            ProgramDir = programDir;
+            
+            DealWithConfig(newConfig);
+        }
+        
+        private void DealWithConfig(Func<TConfig>? newConfig)
+        {
+            if (File.Exists(ConfPath))
+            {
+                using var file = File.OpenText(ConfPath);
+                
+                var serializer = new JsonSerializer();
+                _config = serializer.Deserialize(file, typeof(TConfig)) as TConfig;
+            }
+
+            _config ??= newConfig?.Invoke() ?? new TConfig();
+
+            if (Configuration.SelectOutputDir)
+            {
+                _outputDirDialog.SelectedPath = Directory.Exists(Configuration.LastOutputDir) ? Configuration.LastOutputDir : ProgramDir;
+                if (_outputDirDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Console.WriteLine(_outputDirDialog.SelectedPath);
+                    _useSelectedDir = true;
+                    _selectedDir = _outputDirDialog.SelectedPath;
+                    Configuration.LastOutputDir = _outputDirDialog.SelectedPath;
+                }
+            }
+
+            //TODO Show a config menu
+            using (var file = File.CreateText(ConfPath))
+            {
+                var serializer = new JsonSerializer();
+                serializer.Serialize(file, Configuration);
+            }
+        }
+    }
+}
